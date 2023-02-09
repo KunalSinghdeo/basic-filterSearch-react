20967924e9e7a3adae3b12f2f5f7715af38ee514
KunalSinghdeo <Singhdeokunal11@gmail.com>
2023-02-09 21:53:50 +0530

    filter functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Users } from "./Users";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
   return (
     <div className="App">
-      <header className="App-header">FilterSearch</header>
+      <input
+        placeholder="Search..."
+        type="text"
+        className="search"
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
